23d9d4b8a5be455b35dad67f25f29b63a12e150f
Mukhtarov Sarvarbek <mukhtarovsarvarbek@gmail.com>
2023-12-05 11:43:42 +0500

    Initial commit

---
+++ src/main/java/com/example/SifatliTalimBotApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SifatliTalimBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SifatliTalimBotApplication.class, args);
+	}
+
+}
