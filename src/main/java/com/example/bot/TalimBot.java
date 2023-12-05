ebec92ad72ee423298956708b6b507571bf75349
Mukhtarov Sarvarbek <mukhtarovsarvarbek@gmail.com>
2023-12-05 11:49:51 +0500

    initial

---
+++ src/main/java/com/example/bot/TalimBot.java
@@ -0,0 +1,9 @@
+package com.example.bot;
+
+/**
+ * @author 'Mukhtarov Sarvarbek' on 05.12.2023
+ * @project sifatli_talim_bot
+ * @contact @sarvargo
+ */
+public class TalimBot {
+}
---
+++ src/main/java/com/example/web/config/security/SecurityConfiguration.java
@@ -0,0 +1,12 @@
+package com.example.web.config.security;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 'Mukhtarov Sarvarbek' on 05.12.2023
+ * @project sifatli_talim_bot
+ * @contact @sarvargo
+ */
+@Configuration
+public class SecurityConfiguration {
+}
