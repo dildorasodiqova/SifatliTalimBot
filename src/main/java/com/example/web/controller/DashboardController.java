575c9d44e0dd06655a5df0baef524dc2c79861e5
Mukhtarov Sarvarbek <mukhtarovsarvarbek@gmail.com>
2023-12-11 16:42:38 +0500

    dashboard and login

---
+++ src/main/java/com/example/web/controller/DashboardController.java
@@ -0,0 +1,23 @@
+package com.example.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author 'Mukhtarov Sarvarbek' on 11.12.2023
+ * @project sifatli_talim_bot
+ * @contact @sarvargo
+ */
+@Controller
+@RequestMapping("/dashboard")
+@RequiredArgsConstructor
+public class DashboardController {
+
+    @GetMapping("")
+    public String dashboard(Model model){
+        return "dashboard/index";
+    }
+}
---
+++ src/main/resources/static/js/dashboard/script.js
@@ -0,0 +1,66 @@
+// setup
+const data = {
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    datasets: [{
+        label: 'Weekly Sales',
+        data: [18, 12, 6, 9, 12, 3, 9],
+        backgroundColor: [
+            'rgba(255, 26, 104, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(0, 0, 0, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 26, 104, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(0, 0, 0, 1)'
+        ],
+        borderWidth: 1
+    }]
+};
+
+// config
+const config = {
+    type: 'bar',
+    data,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+};
+
+// render init block
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
+
+
+const labels = ['1-dekabr', '2-dekabr', '3-dekabr', '4-dekabr', '5-dekabr', '6-dekabr', '7-dekabr'];
+const data1 = {
+    labels: labels,
+    datasets: [
+        {
+            label: 'Userlar',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: true,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+        }
+    ]
+};
+const config1 = {
+    type: 'line',
+    data: data1,
+};
+new Chart(document.getElementById('myChart1'), config1)
